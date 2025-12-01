a7fd96a9b7b7772f1e23561eba00caa38b666a79
Flacooo10 <chahid.lmahfoud@edu.esiee-it.fr>
2025-12-01 14:19:54 +0100

    Update variables.c

---
+++ TP1/src/variables.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+
+    // char
+    signed char s_char = -50;
+    unsigned char u_char = 200;
+
+    // short
+    signed short s_short = -30000;
+    unsigned short u_short = 60000;
+
+    // int
+    signed int s_int = -123456;
+    unsigned int u_int = 123456;
+
+    // long int
+    signed long int s_long = -1234567890L;
+    unsigned long int u_long = 1234567890UL;
+
+    // long long int
+    signed long long int s_ll = -1234567890123LL;
+    unsigned long long int u_ll = 1234567890123ULL;
+
+    // flottants
+    float f = 3.14f;
+    double d = 3.14159265;
+    long doub
